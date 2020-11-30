bacdbfc98f34053c4e18f1d65bc63504099a770e
Herman Obst Demaestri <herman.obst@lambdaclass.com>
2020-11-30 13:41:25 -0300

    linear trend method added

--- time-series/exponential_smooth.jl
+++ time-series/exponential_smooth.jl
@@ -113,7 +113,7 @@ res = optimize(SES_loss_,lower, upper, initial_x);
 optim = Optim.minimizer(res)
 
 # ╔═╡ 4619edaa-30ea-11eb-3a4c-6721d7443aa0
-pred = SES_weight(0.83378, 446.573, y_)
+pred = SES_weight(0.833, 446.573, y_)
 
 # ╔═╡ e4a13032-30ea-11eb-2f28-672a793137f5
 time = collect(1996:1:2013)
@@ -131,7 +131,7 @@ forecast = SES(y_, 0.83378)
 md"### Holt’s linear trend method"
 
 # ╔═╡ 2230b29c-30f0-11eb-2e02-197ad35d3100
-function HLT(time_serie, α, β, l0, b0)
+function HLT_loss(time_serie, α, β, l0, b0)
 	N = length(time_serie)
 	l_t = 0
 	b_t = 0
@@ -160,9 +160,15 @@ end
 # ╔═╡ ac0f3dfe-30f9-11eb-17b1-7729d039a6b9
 data = [17.55,21.86,23.89,26.93,26.89,28.83,30.08,30.95,30.19,31.58,32.58,33.48,39.02,41.39,41.60, 44.66,46.95,48.73,51.49,50.03,60.64,63.36,66.36,68.20,68.12,69.78,72.60]
 
+# ╔═╡ 1cd57bf6-3317-11eb-3b83-9f1872df41c9
+time_ = collect(1990:2016)
+
+# ╔═╡ fa5ffb46-3316-11eb-3d31-978b6c604077
+plot(time_, data, legend=false)
+
 # ╔═╡ 06993716-30fa-11eb-0e0a-1f84abee8bf6
 function HLT_loss_(params, time_serie=data)
-	return HLT(time_serie, params[1], params[2], params[3], params[4])
+	return HLT_loss(time_serie, params[1], params[2], params[3], params[4])
 end
 
 # ╔═╡ 7674fda8-30fb-11eb-176b-e5999af211fe
@@ -177,6 +183,93 @@ res1 = optimize(HLT_loss_, lower_, upper_, initial_x_);
 
 # ╔═╡ f9396bd8-30fc-11eb-2c96-f95f493a4270
 optim1 = Optim.minimizer(res1)
+
+# ╔═╡ a4c65e44-331d-11eb-1f52-77139935920a
+HLT_loss(data, 0.8321, 0.0001, 15.57, 2.102)
+
+# ╔═╡ e34f28a8-331d-11eb-398c-adc71cbd3cb5
+HLT_loss(data, 0.8215406645869177, 3.933846947991096e-16, 15.847524289600578, 2.0981489347553435)
+
+# ╔═╡ 63d6d0c4-331f-11eb-08b9-c90eb121d50d
+function HLT(time_serie, α, β, l0, b0)
+	N = length(time_serie)
+	l_t = 0
+	b_t = 0
+	l_t_ = 0 
+	pred = []
+	
+	for i in 1:(N)
+		if i == 1
+			l_t = l0
+			b_t = b0
+		else
+			l_t = time_serie[i - 1] * α + (l_t + b_t) * (1 - α) #b_t "is" b(t-1)
+			b_t = β * (l_t - l_t_) + (1 - β) * b_t
+		end
+		l_t_ = l_t
+		
+		y_pred = l_t + b_t
+		
+		push!(pred, y_pred)
+	end
+	
+	return pred
+
+end
+
+# ╔═╡ 0ac392a0-3320-11eb-2488-eba0765eb0b6
+fit = HLT(data, 0.8321, 0.0001, 15.57, 2.102)
+
+# ╔═╡ 2f5dd4a4-3320-11eb-2de8-51e255bb5c9a
+begin
+plot(data, label="Data", legend=:topleft)
+plot!(fit, label="Fitted")
+end
+
+# ╔═╡ cae17dee-3321-11eb-07b9-8f85a62c807f
+function HLT_forecast(time_serie, α, β, l0, b0, n_pred)
+	N = length(time_serie)
+	l_t = 0
+	b_t = 0
+	l_t_ = 0 
+	pred = []
+	
+	for i in 1:(N)
+		if i == 1
+			l_t = l0
+			b_t = b0
+		else
+			l_t = time_serie[i - 1] * α + (l_t + b_t) * (1 - α) #b_t "is" b(t-1)
+			b_t = β * (l_t - l_t_) + (1 - β) * b_t
+		end
+		l_t_ = l_t
+		
+		y_pred = l_t + b_t	
+	end
+	
+	l_t = time_serie[end] * α + (l_t + b_t) * (1 - α)
+	b_t = β * (l_t - l_t_) + (1 - β) * b_t
+	
+	for i in 1:n_pred
+		y_pred = l_t + b_t * i
+		push!(pred, y_pred)
+	end
+				
+	return vcat(time_serie, pred)
+
+end
+
+# ╔═╡ 93203060-3326-11eb-23b5-794e82e57f95
+data_forecasted = HLT_forecast(data, 0.8321, 0.0001, 15.57, 2.102, 5)
+
+# ╔═╡ 2dab6d5c-3329-11eb-02b7-178ab2f62847
+time_[end]
+
+# ╔═╡ d79228f2-3328-11eb-3026-b3cafeaae72f
+begin
+plot(time_, data_forecasted[1:length(data)], label="Data", legend=:topleft)
+plot!(time_[end]:(time_[end]+5), data_forecasted[length(data):end], label="Forecast")
+end
 
 # ╔═╡ Cell order:
 # ╟─97ceb2ca-2f5a-11eb-2a7b-cd339b881ea1
@@ -203,7 +296,18 @@ optim1 = Optim.minimizer(res1)
 # ╟─df699004-30d7-11eb-18e8-33dc21caf77a
 # ╠═2230b29c-30f0-11eb-2e02-197ad35d3100
 # ╠═ac0f3dfe-30f9-11eb-17b1-7729d039a6b9
+# ╠═1cd57bf6-3317-11eb-3b83-9f1872df41c9
+# ╠═fa5ffb46-3316-11eb-3d31-978b6c604077
 # ╠═06993716-30fa-11eb-0e0a-1f84abee8bf6
 # ╠═7674fda8-30fb-11eb-176b-e5999af211fe
 # ╠═a71787f4-30fc-11eb-39db-e564e728c3db
 # ╠═f9396bd8-30fc-11eb-2c96-f95f493a4270
+# ╠═a4c65e44-331d-11eb-1f52-77139935920a
+# ╠═e34f28a8-331d-11eb-398c-adc71cbd3cb5
+# ╠═63d6d0c4-331f-11eb-08b9-c90eb121d50d
+# ╠═0ac392a0-3320-11eb-2488-eba0765eb0b6
+# ╠═2f5dd4a4-3320-11eb-2de8-51e255bb5c9a
+# ╠═cae17dee-3321-11eb-07b9-8f85a62c807f
+# ╠═93203060-3326-11eb-23b5-794e82e57f95
+# ╠═2dab6d5c-3329-11eb-02b7-178ab2f62847
+# ╠═d79228f2-3328-11eb-3026-b3cafeaae72f
